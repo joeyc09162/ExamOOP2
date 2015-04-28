2ed4a323c1bd4b1d7b1d669fedcd8e3880ccccb4
ase25641 <ase25641@gmail.com>
2015-04-28 17:24:01 +0800

    C AM021968李啟榮提交 updataFile

--- Question2.java
+++ Question2.java
@@ -1,7 +1,15 @@
-﻿public class Question2{
-	public static void main(String[] args){
-		txtCopy("Source.txt", "Dest.txt");
-		editFile("Dest.txt");
-		updateFile("Dest.txt");
+import java.io.*;
+public class Question2{
+	public static void main(String[] args)	throws IOException{
+		//txtCopy("Source.txt", "Dest.txt");
+		//editFile("Dest.txt");
+		//updateFile("Dest.txt");
+		int i;
+		FileReader f=new FileReader("Source.txt");
+		BufferedReader b=new BufferedReader(f);
+		while((i=b.read())!=-1){
+			System.out.println((char)i);
+		}
+		b.close();
 	}
 }
