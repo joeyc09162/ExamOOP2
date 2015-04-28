48d229c0f2a9ba2cd2746a30c1ad041d347ad445
AM021975 <ax655239652229@gmail.com>
2015-04-28 16:25:27 +0800

    A AM021975 陳翌睿提交txtCopy

--- Question2.java
+++ Question2.java
@@ -1,7 +1,28 @@
-﻿public class Question2{
+﻿import java.io.*;
+public class Question2{
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
+	}
+	public static void txtCopy(String a,String b) {
+		FileReader fr;
+		FileWriter fw;
+		BufferedReader bf;
+		String data ="";
+		
+		try{
+			fr = new FileReader(a);
+			bf = new BufferedReader(fr); 
+			fw = new FileWriter(b);
+			while((data=bf.readLine())!=null) {
+				fw.write(data);
+			}
+			fr.close();
+			fw.close();
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+		
 	}
 }
